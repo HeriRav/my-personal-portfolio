9ff266f4d6f95febb93dca4a9cd3f63e458016b0
Heritiana Raveloson <heritiana.rav@gmail.com>
2026-01-15 18:33:31 +0300

    :lipstick: :art: Add disabling into selected language

--- app/[locale]/LocaleSelect.tsx
+++ app/[locale]/LocaleSelect.tsx
@@ -45,7 +45,8 @@ export const LocaleSelect = () => {
             <button
               key={code}
               onClick={() => handleChange(code)}
-              className="px-4 py-2 hover:bg-muted hover:dark:bg-black text-left cursor-pointer flex items-center gap-2"
+              disabled={code === locale}
+              className="px-4 py-2 hover:bg-muted hover:dark:bg-black text-left cursor-pointer flex items-center gap-2 disabled:opacity-50 disabled:cursor-default disabled:hover:bg-transparent"
             >
               <Flag />
               <span>{label}</span>
